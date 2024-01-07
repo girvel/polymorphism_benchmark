7431b3dd437fd8b3bd4c3646da8e52a46cc8a162
girvel <widauka@ya.ru>
2024-01-07 11:43:46 +0500

    Remember how to do polymorphism in C++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <memory>
 
-int main() {
-    std::cout << "Hello, world!\n";
+class BaseClass {
+public:
+    virtual int raise(int number) {
+        return number + 1;
+    }
+};
+
+class Subclass : public BaseClass {
+public:
+    int raise(int number) override {
+        return number + 2;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    //int operations_n = std::stoi(argv[1]);
+
+    auto raiser = std::make_unique<Subclass>();
+    std::cout << raiser->raise(0) << std::endl;
     return 0;
 }
